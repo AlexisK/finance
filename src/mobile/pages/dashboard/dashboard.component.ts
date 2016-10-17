78cb772f3131df01c963589a021f10d9c9d3c420
Oleksii Kaliuzhnyi <alexey@stuccomedia.com>
2016-10-17 15:12:38 +0300

    dashboard

--- src/mobile/pages/dashboard/dashboard.component.ts
+++ src/mobile/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 
+import {DatabaseService} from 'services';
+
 @Component({
     selector    : 'finance-page-dashboard',
     templateUrl : './dashboard.component.html',
@@ -7,6 +9,15 @@ import {Component} from '@angular/core';
 })
 
 export class DashboardPageComponent {
+
+    constructor(private db: DatabaseService) {
+    }
+
+    get transactions() {
+        return this.db.transactions.sort((a: any, b: any) => {
+            return b.timestamp - a.timestamp;
+        }).slice(0, 5);
+    }
 }
 
 export const route = {path : '', component : DashboardPageComponent};
